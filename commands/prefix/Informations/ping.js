5f0ef41e583039c0b4839e3d2c8c4f41a6b2bff6
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-11-10 23:40:51 -0300

    Create ping.js

---
+++ commands/prefix/Informations/ping.js
@@ -0,0 +1,12 @@
+module.exports = {
+  structure: {
+    name: 'ping',
+    description: 'Latência da WebSocket do bot.',
+    aliases: ['latencia', 'latency']
+  },
+  run: async (client, message, args) => {
+    message.reply({
+      content: `**( 🏓 ) › Pong.**\n>>> **Websocket:**\`${client.ws.ping}ms\`\n**Tempo de execução:** \`${Date.now() - message.createdTimestamp}ms\``
+    });
+  }
+}
