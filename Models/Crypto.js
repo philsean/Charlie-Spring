2fa034176a39e2c26ee88171c711dbc63d21a58d
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-11-11 08:17:52 -0300

    Update Crypto.js

--- Models/Crypto.js
+++ Models/Crypto.js
@@ -2,6 +2,10 @@ const { model, Schema } = require('mongoose');
 
 module.exports = model('crypto',
   new Schema({
-    
+    bolsa: {
+      bitcoin: { type: [[Number]], default: [ 136466 ] },
+      ethereum: { type: [[Number]], default: [ 27399 ] },
+      litecoin: { type: [[Number]], default: [ 3545 ] }
+    }
   })
 );
