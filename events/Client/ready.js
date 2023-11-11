c445dfffbc61f2b673684768ca430a83b12beff0
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-11-11 09:03:02 -0300

    Update ready.js

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -7,7 +7,7 @@ module.exports = {
   run: async (_, client) => {
     log('Logado em: ' + client.user.tag, 'done');
     let user = await client.users.fetch('916712541797896263');
-    user.send({ content: `( ✏️ ) › Fui reiniciando e parece que tem coisa nova no código. ( <t:${~~(Date.now() / 1000)}:F> )` });
+    // user.send({ content: `( ✏️ ) › Fui reiniciando e parece que tem coisa nova no código. ( <t:${~~(Date.now() / 1000)}:F> )` });
     let guilds = ['1077563002733219962', '995335063975768115', '985625402288529449', '1146220629230633071'];
     
     guilds.forEach(async (id) => {
