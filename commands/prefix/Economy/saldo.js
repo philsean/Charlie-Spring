5792e7285f55d81a260fc266c626d082efd2b317
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-11-12 13:43:10 -0300

    Update saldo.js

--- commands/prefix/Economy/saldo.js
+++ commands/prefix/Economy/saldo.js
@@ -11,10 +11,10 @@ module.exports = {
   run: async (client, message, args, db) => {
     let user = client.findUser(args.join(' '), message);
     user = user.id === client.user.id ? message.author : user;
-    let data = await db.find({});
-    let dataU = data.find(x => x._id === user.id);
+    let data = await client.database.getLeaderboard();
+    let dataU = data.leaderboard.find(x => x._id === user.id);
     let money = dataU?.economy?.sapphire || 0;
 
-    message.reply({ content: `( ${client.emoji.sapphire} ) › ${message.author}, ${message.author.id === user.id ? 'você' : `**${user.globalName || user.username}**`} possui **${money} Sapphire(s)**, e está na posição **#${data.indexOf(dataU) >= 0 ? data.indexOf(dataU) + 1 : '+100'}** no ranking.` });
+    message.reply({ content: `( ${client.emoji.sapphire} ) › ${message.author}, ${message.author.id === user.id ? 'você' : `**${user.globalName || user.username}**`} possui **${money.toLocaleString('pt-br')} Sapphire(s)**, e está na posição **#${data.indexOf(dataU) >= 0 ? data.indexOf(dataU) + 1 : '+100'}** no ranking.` });
   }
 }
