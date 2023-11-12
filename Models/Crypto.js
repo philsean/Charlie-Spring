120c02b234e86000e5f709953a79c81a2d67cca1
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-11-12 10:23:38 -0300

    Update Crypto.js

--- Models/Crypto.js
+++ Models/Crypto.js
@@ -4,9 +4,9 @@ module.exports = model('crypto',
   new Schema({
     _id: String,
     bolsa: {
-      bitcoin: { type: [[Number]], default: [ 136466 ] },
-      ethereum: { type: [[Number]], default: [ 27399 ] },
-      litecoin: { type: [[Number]], default: [ 3545 ] }
+      bitcoin: { type: Array, default: [ 136466 ] },
+      ethereum: { type: Array, default: [ 27399 ] },
+      litecoin: { type: Array, default: [ 3545 ] }
     }
   })
 );
