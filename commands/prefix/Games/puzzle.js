1ae83da5f6e793b9c53e08b02345e9d219c5a66d
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-12-19 07:31:34 -0300

    Atualizar o puzzle.js

--- commands/prefix/Games/puzzle.js
+++ commands/prefix/Games/puzzle.js
@@ -3,6 +3,7 @@ const Puzzle = require('../../../games/Puzzle');
 module.exports = {
   structure: {
     name: 'puzzle',
+    description: 'Resolva o quebra cabeça.',
     aliases: ['quebra-cabeça'],
     developer: true
   },
