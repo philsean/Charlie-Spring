3ac8f39dbc205623d4059fd690faad68df9891e3
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-12-19 21:52:08 -0300

    Atualizar o puzzle.js

--- commands/prefix/Games/puzzle.js
+++ commands/prefix/Games/puzzle.js
@@ -12,8 +12,8 @@ module.exports = {
     let game = client.games.puzzle.get(message.author.id);
     if (game) return message.reply(`( ${client.emoji.x} ) › Já tem uma partida sua rolando: https://discord.com/channels/${game.display.guild.id}/${game.display.channel.id}/${game.display.id}`);
     
-    let withoutTip = new EmbedBuilder().setCustomId('without').setLabel('Começar.').setStyle(ButtonStyle.Secondary);
-    let withTip = new EmbedBuilder().setCustomId('with').setLabel('Começar com dicas.').setStyle(ButtonStyle.Primary);
+    let withoutTip = new ButtonBuilder().setCustomId('without').setLabel('Começar.').setStyle(ButtonStyle.Secondary);
+    let withTip = new ButtonBuilder().setCustomId('with').setLabel('Começar com dicas.').setStyle(ButtonStyle.Primary);
 
     message.reply({ content: '**( 🧩 ) › Puzzle `( Quebra-cabeça )`**\n>>> O jogo consiste em você colocar as peças em seus lugares correspondentes, ao conseguir resolver um puzzle você ganha algo aleatório decidido pelo bot. Claro, quanto mais você joga, mais as chances de você ganhar algo bom aumenta.\n\n*Clique no botão abaixo para iniciar um puzzle, e boa sorte.*', components: [new ActionRowBuilder().addComponents(withoutTip, withTip)] });
   }
