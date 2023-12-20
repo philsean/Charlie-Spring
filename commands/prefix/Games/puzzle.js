936b585fb191f491671e5be408a4d61cace819a2
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-12-19 23:43:26 -0300

    Update puzzle.js

--- commands/prefix/Games/puzzle.js
+++ commands/prefix/Games/puzzle.js
@@ -12,9 +12,22 @@ module.exports = {
     let game = client.games.puzzle.get(message.author.id);
     if (game) return message.reply(`( ${client.emoji.x} ) › Já tem uma partida sua rolando: https://discord.com/channels/${game.display.guild.id}/${game.display.channel.id}/${game.display.id}`);
     
-    let withoutTip = new ButtonBuilder().setCustomId('without').setLabel('Começar.').setStyle(ButtonStyle.Secondary);
-    let withTip = new ButtonBuilder().setCustomId('with').setLabel('Começar com dicas.').setStyle(ButtonStyle.Primary);
+    let withoutTip = new ButtonBuilder().setCustomId('false').setLabel('Começar.').setStyle(ButtonStyle.Secondary);
+    let withTip = new ButtonBuilder().setCustomId('true').setLabel('Começar com dicas.').setStyle(ButtonStyle.Primary);
 
-    message.reply({ content: '**( 🧩 ) › Puzzle `( Quebra-cabeça )`**\n>>> O jogo consiste em você colocar as peças em seus lugares correspondentes, ao conseguir resolver um puzzle você ganha algo aleatório decidido pelo bot. Claro, quanto mais você joga, mais as chances de você ganhar algo bom aumenta.\n\n*Clique no botão abaixo para iniciar um puzzle, e boa sorte.*', components: [new ActionRowBuilder().addComponents(withoutTip, withTip)] });
+    game = new Puzzle({ client, message });
+    client.games.puzzle.set(message.author.id, game);
+    
+    message.reply({ content: '**( 🧩 ) › Puzzle `( Quebra-cabeça )`**\n>>> O jogo consiste em você colocar as peças em seus lugares correspondentes, ao conseguir resolver um puzzle você ganha algo aleatório decidido pelo bot. Claro, quanto mais você joga, mais as chances de você ganhar algo bom aumenta.\n\n*Clique no botão abaixo para iniciar um puzzle, e boa sorte.*', components: [new ActionRowBuilder().addComponents(withoutTip, withTip)] }).then((q) => {
+      let filter = (i) => i.user.id === message.author.id;
+      let quest = q.createMessageComponentCollector({ filter, componentType: ComponentType.Button, time: 15000, max: 1, errors: ['time'] });
+
+      quest.on('collect', (i) => {
+        game.start(Boolean(i.customId));
+      });
+      quest.on('end', => {
+        game.end();
+      });
+    });
   }
 }
