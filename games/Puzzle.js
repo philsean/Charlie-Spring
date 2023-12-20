3b9f06179f572f10547ef58e5cb40781d15ed5a0
Sean S2 <140343656+starryoceanSean@users.noreply.github.com>
2023-12-20 09:02:08 -0300

    Atualizar o Puzzle.js

--- games/Puzzle.js
+++ games/Puzzle.js
@@ -60,7 +60,7 @@ module.exports = class Puzzle {
         let emoji = this.puzzle.emojis[x.split('_')[1]].replace('>', '').split(':')[2];
         let b = new ButtonBuilder()
           .setCustomId(`puzzle_${i}`)
-          .setStyle(this.in.tip ? (this.in.table[i] === this.in.solved[i] ? ButtonStyle.Success : ButtonStyle.Secondary) : (i.endsWith(String(this.moving.on)) ? ButtonStyle.Primary : ButtonStyle.Secondary))
+          .setStyle(this.in.tip ? (this.in.table[i] === this.in.solved[i] ? ButtonStyle.Success : ButtonStyle.Secondary) : (x.endsWith(String(this.moving.on)) ? ButtonStyle.Primary : ButtonStyle.Secondary))
           .setEmoji(emoji);
 
         rows[Math.floor(i / 4)] = rows[Math.floor(i / 4)].addComponents(b);
